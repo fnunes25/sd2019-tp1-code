6699fd33396ee9e03ba265106eb069ff63d84d72
octaviojardim <octaviojardim@10-22-109-110.ed2.eduroam.fct.unl.pt>
2019-04-23 09:54:20 +0100

    version 1.1

--- SD2019-Microgram-Code/src/discovery/Discovery.java
+++ SD2019-Microgram-Code/src/discovery/Discovery.java
@@ -1,9 +1,13 @@
 package discovery;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.logging.Logger;
 
 public class Discovery {
@@ -54,9 +58,58 @@ public class Discovery {
 	 * @param  serviceName the name of the service being discovered
 	 * @param  minRepliesNeeded the required number of service replicas to find. 
 	 * @return an array of URI with the service instances discovered. Returns an empty, 0-length, array if the service is not found within the alloted time.
+	 * @throws IOException 
+	 * @throws URISyntaxException 
 	 * 
 	 */
-	public static URI[] findUrisOf(String serviceName, int minRepliesNeeded) {
-		throw new Error("Not Implemented...");
+	public static URI[] findUrisOf(String serviceName, int minRepliesNeeded) throws IOException, URISyntaxException {
+		int counterUris = 0;
+		final int MAX_DATAGRAM_SIZE = 65536;
+		String[] uriArray = new String[100];
+		boolean isDiferent = true;
+		URI[] finalArray = new URI[minRepliesNeeded];
+
+		final InetAddress group = InetAddress.getByName("226.226.226.226");
+		try (MulticastSocket socket = new MulticastSocket(2266)) {
+			socket.joinGroup(group);
+
+			long startTime = System.currentTimeMillis();
+
+			while (counterUris < minRepliesNeeded) {
+
+				byte[] buffer = new byte[MAX_DATAGRAM_SIZE];
+				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+
+				socket.setSoTimeout(5000);
+				socket.receive(request);
+
+				String line = new String(request.getData(), 0, request.getLength());
+				String[] newLine = line.split(DELIMITER);
+				String name = newLine[0];
+				String uri = newLine[1];
+
+				if (counterUris > 0) {
+					for (String currentUri : uriArray) {
+						if (uri.equals(currentUri)) {
+							isDiferent = false;
+						}
+					}
+				}
+
+				if (serviceName.equals(name) && isDiferent == true) {
+					uriArray[counterUris] = uri;
+					URI myUri = new URI(uri);
+					finalArray[counterUris++] = myUri;
+				}
+			}
+
+		}
+
+		// set set so timeout antes do receive 5000 !!!!
+		// guadar empo ao entrar na funcao
+		// start time = time
+		// ver quanto tempo ja pasoou , so quremos etsra 5 seundos. entro 3
+
+		return finalArray;
 	}	
 }
---
+++ SD2019-Microgram-Code/src/microgram/impl/srv/rest/RestMediaResources.java
@@ -0,0 +1,40 @@
+package microgram.impl.srv.rest;
+
+import javax.ws.rs.WebApplicationException;
+
+import microgram.api.java.Result;
+import microgram.api.rest.RestMediaStorage;
+import microgram.impl.srv.java.JavaMedia;
+
+public class RestMediaResources extends RestResource implements RestMediaStorage {
+
+
+	
+	final String baseUri;
+	final JavaMedia impl;
+	
+	public RestMediaResources(String baseUri ) {
+		this.baseUri = baseUri + RestMediaStorage.PATH;
+		this.impl = new JavaMedia();
+	}
+	
+	@Override
+	public String upload(byte[] bytes) {
+		Result<String> result = impl.upload(bytes);
+		if (result.isOK())
+			return baseUri + "/" + result.value();
+		else
+			throw new WebApplicationException(super.statusCode(result));
+	}
+
+	@Override
+	public byte[] download(String id) {
+		Result<byte[]> result = impl.download( id );
+		if( result.isOK() )
+			return result.value();
+		else
+			throw new WebApplicationException( super.statusCode( result )) ;
+ 	}
+
+	
+}
